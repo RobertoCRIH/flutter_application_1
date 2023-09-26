07afdda5917f9fa1ea70773e1d94a15a98710ab4
RobertoCRIH <108534863+RobertoCRIH@users.noreply.github.com>
2023-09-26 07:59:51 -0600

    widget

---
+++ lib/widgets/auth_background.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AuthBackground extends StatelessWidget {
+  final Widget child;
+
+  const AuthBackground({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Stack(
+        children: [this.child],
+      ),
+    );
+  }
+}
