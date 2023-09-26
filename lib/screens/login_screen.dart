a4f688f9b092d88d885f4fad0fa36e434911afc7
RobertoCRIH <108534863+RobertoCRIH@users.noreply.github.com>
2023-09-26 07:25:35 -0600

    Initial commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Stack(
+        children: [_loginForm()],
+      )),
+    );
+  }
+
+  Widget _loginForm() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextField(
+          controller: _usernameController,
+          decoration: const InputDecoration(
+              icon: Icon(Icons.mail), labelText: 'Type your username.'),
+        ),
+        TextField(
+          controller: _passwordController,
+          decoration: const InputDecoration(
+              icon: Icon(Icons.lock_open), labelText: 'Type your password.'),
+          obscureText: true,
+          keyboardType: TextInputType.visiblePassword,
+        ),
+        ElevatedButton(onPressed: _login, child: const Text('Login'))
+      ],
+    );
+  }
+
+  void _login() {
+    final username = _usernameController.text.trim();
+    final password = _passwordController.text.trim();
+
+    print('username: $username');
+    print('password: $password');
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:flutter_application_1/screens/home_screen.dart';
+export 'package:flutter_application_1/screens/login_screen.dart';
